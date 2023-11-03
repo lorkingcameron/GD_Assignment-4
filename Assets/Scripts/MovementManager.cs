94db86ea3270dc9947b1b5e310ccf39896994a3a
lorkingcameron <lorkingcameron@gmail.com>
2023-11-03 14:03:28 +1100

    Assessment 3 Catchup

--- Assets/Scripts/MovementManager.cs
+++ Assets/Scripts/MovementManager.cs
@@ -25,25 +25,25 @@ public class MovementManager : MonoBehaviour
         audioSource.Play();
         audioSource.loop = true;
 
-        moves = new List<Vector3>();
-        moves.Add(new Vector3(5.0f, 0.0f, 0.0f));
-        moves.Add(new Vector3(0.0f, -4.0f, 0.0f));
-        moves.Add(new Vector3(-5.0f, 0.0f, 0.0f));
-        moves.Add(new Vector3(0.0f, 4.0f, 0.0f));
-        movesIndex = 0;
+        // moves = new List<Vector3>();
+        // moves.Add(new Vector3(5.0f, 0.0f, 0.0f));
+        // moves.Add(new Vector3(0.0f, -4.0f, 0.0f));
+        // moves.Add(new Vector3(-5.0f, 0.0f, 0.0f));
+        // moves.Add(new Vector3(0.0f, 4.0f, 0.0f));
+        // movesIndex = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (tweener.AddTween(item.transform, item.transform.position, item.transform.position + moves[movesIndex])) {
+        // if (tweener.AddTween(item.transform, item.transform.position, item.transform.position + moves[movesIndex])) {
             
-            var dir = movesIndex == 0 ? "Right" :
-                movesIndex == 1 ? "Down" :
-                movesIndex == 2 ? "Left" : "Up";
-            anim.SetTrigger(dir);
-            movesIndex++;
-            movesIndex = (movesIndex == 4) ? 0 : movesIndex;
-        }
+        //     var dir = movesIndex == 0 ? "Right" :
+        //         movesIndex == 1 ? "Down" :
+        //         movesIndex == 2 ? "Left" : "Up";
+        //     anim.SetTrigger(dir);
+        //     movesIndex++;
+        //     movesIndex = (movesIndex == 4) ? 0 : movesIndex;
+        // }
     }
 }
